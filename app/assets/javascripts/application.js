4d05d52e687c3360bfea5310b6752d5e0f7cd815
ltackett <lorin.tackett@gmail.com>
2014-07-26 04:56:45 -0700

    Hm. Ember wasn't quite working out of the box.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,19 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require handlebars
+//= require ember
+
+//= require_tree ./templates
+//= require_self
+
+window.ltackett = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_BINDINGS: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_STACKTRACE_ON_DEPRECATION: true,
+  LOG_VERSION: true,
+
+  debugMode:   true,
+  rootElement: '#ltackett'
+});
